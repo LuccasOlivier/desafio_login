31b0ded24b1389d08bf3920207b1853e186de816
LuccasOlivier <lukashamashia@gmail.com>
2025-02-21 13:10:56 -0300

    Add project files

---
+++ desafio_login/accounts/views.py
@@ -0,0 +1,87 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+import re
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib import messages
+
+
+# View para a página de login
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if not email or not password:
+            messages.error(request, 'E-mail e senha são obrigatórios.')
+            return redirect('login')  # Retorna para a página de login
+
+        user = authenticate(request, username=email, password=password)
+
+        # Mensagens de erro personalizadas
+        if not User.objects.filter(username=email).exists():
+            messages.error(request, 'E-mail inexistente.')
+        elif user is None:
+            messages.error(request, 'Senha inválida.')
+        else:
+            login(request, user)
+            return redirect('menu')
+
+    # Se não for uma requisição POST, renderiza a página normalmente
+    return render(request, 'login.html')
+
+
+# View para a página de registro
+def register_view(request):
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['senha']
+        confirmar_senha = request.POST['confirmar_senha']
+
+        # Validação do nome (apenas letras)
+        if not nome.isalpha():
+            messages.error(request, 'O nome deve conter apenas letras.')
+            return redirect('register')
+
+        # Validação do e-mail
+        if '@' not in email or '.' not in email:
+            messages.error(request, 'E-mail inválido.')
+            return redirect('register')
+
+        # Validação da senha
+        if len(senha) < 8 or not re.search(r'[A-Za-z]', senha) or not re.search(r'[0-9]', senha) or not re.search(r'[\W_]', senha):
+            messages.error(request, 'A senha deve conter pelo menos 8 caracteres, 1 caractere especial, 1 número e 1 letra.')
+            return redirect('register')
+
+        # Verifica se as senhas coincidem
+        if senha != confirmar_senha:
+            messages.error(request, 'As senhas não coincidem.')
+            return redirect('register')
+
+        # Se todas as validações passaram
+        user = User.objects.create_user(username=email, password=senha, first_name=nome)
+        messages.success(request, 'Usuário registrado com sucesso!')
+        return redirect('login')  # Após o registro, redireciona para a tela de login
+
+    return render(request, 'register.html')
+
+# View para a página do menu, acessível apenas para usuários logados
+@login_required
+def menu_view(request):
+    return render(request, 'menu.html')
+
+# View para realizar o logout
+def logout_view(request):
+    
+    logout(request)
+    
+    # Limpar mensagens persistentes da sessão
+    from django.contrib.messages import get_messages
+    get_messages(request)  # limpar qualquer mensagem armazenada na sessão
+    
+    messages.info(request, 'Você foi deslogado com sucesso!')
+    
+    return redirect('login')  # Redireciona para a página de login
---
+++ desafio_login/desafio_login/settings.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-y8acoy+ugl5_ry7=qy7m7jzi0c*-k9yb3u5&m!^2e2=e$k-s26'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'tailwind',
+    'accounts',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'desafio_login.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],  # Caminho dos templates
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'desafio_login.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = 'menu'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "theme/static",  # Aqui
+]
+
+TAILWIND_APP_NAME = 'theme'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
