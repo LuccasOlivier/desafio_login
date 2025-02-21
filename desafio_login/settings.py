7e1a7dbba2849ab88ad2dc53818e5bc05a6f7184
LuccasOlivier <lukashamashia@gmail.com>
2025-02-21 14:32:27 -0300

    Adicionado ALLOWED_HOSTS = ['*'] no settings.py

--- desafio_login/settings.py
+++ desafio_login/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-y8acoy+ugl5_ry7=qy7m7jzi0c*-k9yb3u5&m!^2e2=e$k-s26'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  
 
 INSTALLED_APPS = [
     'django.contrib.admin',
